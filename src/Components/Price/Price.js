5628fa5ee9fa3d7ad1cdeb90388d879789b8d537
MehediHasanNabil <63330618+MehediHasanNabil@users.noreply.github.com>
2020-09-21 14:34:03 +0600

    Add files via upload

--- src/Components/Price/Price.js
+++ src/Components/Price/Price.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Price = (props) => {
+    const totalPrice = props.totalPrice;
+    return (
+        <div>
+            <h3><b>Total Price: ${totalPrice.toFixed(2)}</b></h3>
+        </div>
+    );
+};
+
+export default Price;
